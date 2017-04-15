7f3964fb0421d50ceb29b95c5057b327ebe54555
Administrator <email@e1.ru>
2017-04-15 15:42:43 +0300

    test

--- bot.js
+++ bot.js
@@ -31,10 +31,11 @@ js.include = function (path) {
 
 
 if ( !js.include('Tabs') ) throw new Error("Module include error: Tabs");
+Tabs.go(2);
 //if ( !js.include('Proxy') ) throw new Error("Module include error: Proxy");
 //if ( !js.include('Folders') ) throw new Error("Module include error: Folders");
 //if ( !js.include('BaseOfAnswers') ) throw new Error("Module include error: BaseOfAnswers");
 
 
-Tabs.go(2);
+
 
