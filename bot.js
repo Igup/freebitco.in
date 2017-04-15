e7bd2c8f8fa9598fd6629e9f5de45958b2783d1a
Administrator <email@e1.ru>
2017-04-15 13:36:02 +0300

    test

--- bot.js
+++ bot.js
@@ -31,7 +31,7 @@ js.include = function (path) {
 };
 
 
-if ( !js.include('Tabs2')) throw new Error("stop");
+if ( !js.include('Tabs2')) throw new Error("Error module includ: Tabs");
 iimDisplay("Hello");
 /*
 try {
