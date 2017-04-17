c44c34911fe527ba753f9ca45618d0aa224c04cd
Administrator <email@e1.ru>
2017-04-17 08:13:46 +0300

    test

---
+++ Maintenance/Folders.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Igup on 15.04.2017.
+ */
+
+js.module = function (path) {
+    js.loadedModules[path] = true;
+};
+js.module("Folders");
+
+//Определение путей
+function Folders() {
+
+    var ds = function () {
+        iimPlayCode(`SET !EXTRACT {{!FOLDER_DATASOURCE}}`);
+        return iimGetExtract();
+    }();
+    var iMacros = ds.substring(0, ds.toLowerCase().indexOf('datasources'));
+
+    this.downloads = iMacros + "Downloads";
+    this.macros = iMacros + "Macros";
+    this.datasources = ds;
+}
