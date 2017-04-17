eabd7ec097c44fa2abddf05621beb7204693ab07
Administrator <email@e1.ru>
2017-04-17 16:55:36 +0300

    test

--- Maintenance/BaseOfAnswers.js
+++ Maintenance/BaseOfAnswers.js
@@ -30,7 +30,7 @@ function BaseOfAnswers () {
 BaseOfAnswers.prototype.valueToKey = function () {
     alert("Hello");
     for (var ans in this) {
-        alert(ans);
+        alert(BaseOfAnswers[ans]);
         //var val = this.key;
         //this.val = key;
         //delete BaseOfAnswers.ans;
