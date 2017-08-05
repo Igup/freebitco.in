143361e2b486127fac3bc7dce75293046e8a11d3
Igup <email@e1.ru>
2017-08-05 18:25:24 +0500

    test

--- Maintenance/ReCaptcha2.js
+++ Maintenance/ReCaptcha2.js
@@ -12,7 +12,9 @@ js.module('ReCaptcha2');
 // Конструктор родителя пишет свойства конкретного объекта
 function ReCaptcha2() {
 
-    this.apiKey			 = false;
+    this.key			 = false;
+
+    this.soft_id         = false;
     this.macrosHeader	 = false;
     this.answer			 = {
         isSolved: false,
@@ -26,9 +28,57 @@ function ReCaptcha2() {
 
 // Методы хранятся в прототипе
 ReCaptcha2.prototype.getAnswer = function () {
-    if (window.document.querySelector('.g-recaptcha') == null) {
+    //Проверка, что на странице есть Recaptcha
+    if ( window.document.querySelector('.g-recaptcha') === null ) {
         this.answer.hasError = true;
         this.answer.errorText = "Recaptcha not found on page.";
+        return false;
+    }
+    //Проверка, что ruCaptcha apiKey определен
+    if (this.key) {
+    } else {
+        this.answer.hasError = true;
+        this.answer.errorText = "ruCaptcha apiKey not defined.";
+        return false;
+    }
+    //Проверка, что soft_id определен
+    var soft_id = "";
+    if (this.soft_id) {
+        soft_id = "soft_id=" + this.soft_id
+    }
+
+    this.googlekey  = window.document.querySelector('.g-recaptcha').getAttribute('data-sitekey');
+    this.pageurl = window.location.host;
+
+    var params = "key=" + this.key + "&method=userrecaptcha&googlekey=" + this.googlekey + "&pageurl=" + this.pageurl + "&json=true&header_acao=1" + soft_id;
+    var XMLHttpRequest = Components.Constructor("@mozilla.org/xmlextras/xmlhttprequest;1");
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', "http://rucaptcha.com/in.php", false);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    xhr.timeout = 60000;
+
+    try {
+        xhr.send(params);
+        if (xhr.status !== 200) {
+            this.answer.hasError = true;
+            this.answer.errorText = "An error occurred while loading ReCaptcha2 answer from ruCaptcha" + xhr.status;
+            return false;
+        } else {
+            var res = JSON.parse(xhr.responseText);
+            if (res.status == 1) {
+                //todo добавить получение ответа
+                window.console.log(params);
+            } else {
+                this.answer.hasError = true;
+                this.answer.errorText = "An error occurred while solving ReCaptcha2 by ruCaptcha" + xhr.status;
+                return false;
+            }
+        }
+    } catch (e) {
+        this.answer.hasError = true;
+        this.answer.errorText = e.name;
+        return false;
     }
 
 }
