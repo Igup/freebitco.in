b156cac1ed10eec50fdada020ac4be80b6d8d477
Igup <email@e1.ru>
2017-08-07 19:38:53 +0500

    test

--- Maintenance/ElementScreenshot.js
+++ Maintenance/ElementScreenshot.js
@@ -8,46 +8,45 @@ js.module = function (path) {
 js.module("ElementScreenshot");
 
 ElementScreenshot = {
-
-    mainWindow: window.QueryInterface(Components.interfaces.nsIInterfaceRequestor)
-        .getInterface(Components.interfaces.nsIWebNavigation)
-        .QueryInterface(Components.interfaces.nsIDocShellTreeItem)
-        .rootTreeItem
-        .QueryInterface(Components.interfaces.nsIInterfaceRequestor)
-        .getInterface(Components.interfaces.nsIDOMWindow),
-    _findPosX: function (obj) {
-        var curleft = 0;
-        if (obj.offsetParent) {
-            while (1) {
-                curleft += obj.offsetLeft;
-                if (!obj.offsetParent) {
-                    break;
-                }
-                obj = obj.offsetParent;
-            }
-        } else if (obj.x) {
-            curleft += obj.x;
-        }
-        return curleft;
-    }(),
-    _findPosY: function (obj) {
-        var curtop = 0;
-        if (obj.offsetParent) {
-            while (1) {
-                curtop += obj.offsetTop;
-                if (!obj.offsetParent) {
-                    break;
-                }
-                obj = obj.offsetParent;
-            }
-        } else if (obj.y) {
-            curtop += obj.y;
-        }
-        return curtop;
-    }(),
     Base64: function (elm) {
-        var x = findPosX(elm);
-        var y = findPosY(elm);
+        _mainWindow = window.QueryInterface(Components.interfaces.nsIInterfaceRequestor)
+            .getInterface(Components.interfaces.nsIWebNavigation)
+            .QueryInterface(Components.interfaces.nsIDocShellTreeItem)
+            .rootTreeItem
+            .QueryInterface(Components.interfaces.nsIInterfaceRequestor)
+            .getInterface(Components.interfaces.nsIDOMWindow);
+        _findPosX = function (obj) {
+            var curleft = 0;
+            if (obj.offsetParent) {
+                while (1) {
+                    curleft += obj.offsetLeft;
+                    if (!obj.offsetParent) {
+                        break;
+                    }
+                    obj = obj.offsetParent;
+                }
+            } else if (obj.x) {
+                curleft += obj.x;
+            }
+            return curleft;
+        }
+        _findPosY = function (obj) {
+            var curtop = 0;
+            if (obj.offsetParent) {
+                while (1) {
+                    curtop += obj.offsetTop;
+                    if (!obj.offsetParent) {
+                        break;
+                    }
+                    obj = obj.offsetParent;
+                }
+            } else if (obj.y) {
+                curtop += obj.y;
+            }
+            return curtop;
+        }
+        var x = _findPosX(elm);
+        var y = _findPosY(elm);
         var width = elm.clientWidth;
         var height = elm.clientHeight;
         var cnvs = window.document.createElement("canvas");
@@ -58,7 +57,7 @@ ElementScreenshot = {
         var ctx = cnvs.getContext("2d");
         // To take a snapshot of entire window
         // ctx.drawWindow(mainWindow.content, 0, 0, mainWindow.innerWidth, mainWindow.innerHeight, "rgb(255,255,255)");
-        ctx.drawWindow(mainWindow.content, x, y, width, height, "rgb(255,255,255)");
+        ctx.drawWindow(_mainWindow.content, x, y, width, height, "rgb(255,255,255)");
 
         var cnvs2 = window.document.createElement("canvas");
         cnvs2.width = 266;
@@ -66,7 +65,7 @@ ElementScreenshot = {
         ctx = cnvs2.getContext("2d");
         ctx.drawImage(cnvs, 0, 0, cnvs2.width, cnvs2.height);
 
-        return (cnvs2.toDataURL());
+        return (cnvs2.toDataURL().replace(/^data:image\/(png|jpg);base64,/, ""));
         //return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
     }
 }
